8bb17164a072b3117ca2995cc5ddc6bafa6be671
lromang <luis.roangarci@gmail.com>
2015-11-11 11:42:29 -0600

    2015-11-11

--- Datasets/webService.R
+++ Datasets/webService.R
@@ -4,4 +4,5 @@
 source("/home/luis/Documents/Presidencia/MiningDatosGob/Datasets/ultimate.R")
 
 filter_data <- dplyr::select(all_data, one_of("dep", "slug", "conj","rec","rec_des","rec_url"))
+filter_data <- dplyr::filter(filter_data, slug != "NA")
 write.csv(filter_data, "./filter_data.csv", row.names = FALSE)
