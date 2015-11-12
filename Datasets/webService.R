02fc0a3f61dbcfe71ea429540ff59c773260c93b
lromang <luis.roangarci@gmail.com>
2015-11-12 11:42:33 -0600

    2015-11-12

--- Datasets/webService.R
+++ Datasets/webService.R
@@ -1,6 +1,6 @@
 #! /usr/bin/Rscript
 
-#source("/home/luis/quickStats/MiningDatosGob/Datasets/ultimate.R")
+##source("/home/luis/quickStats/MiningDatosGob/Datasets/ultimate.R")
 source("/home/luis/Documents/Presidencia/MiningDatosGob/Datasets/ultimate.R")
 
 filter_data <- dplyr::select(all_data, one_of("dep", "slug", "conj","rec","rec_des","rec_url"))
