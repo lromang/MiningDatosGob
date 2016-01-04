bc408972c082f3bb3e2f72a38a73a7b75204b2e0
lromang <luis.roangarci@gmail.com>
2016-01-04 17:41:37 -0600

    urls

--- url_test/url.R
+++ url_test/url.R
@@ -4,22 +4,38 @@
 library(RCurl)
 library(stringr)
 library(plyr)
+library(tidyr)
+library(R.utils)
+
 ###################################
 ### Obtencion de URLs
 ###################################
-data <- read.csv("https://raw.githubusercontent.com/lromang/MiningDatosGob/master/Datasets/MAT.csv", stringsAsFactors = FALSE)
+data <- read.csv("https://raw.githubusercontent.com/lromang/MiningDatosGob/master/Datasets/MAT.csv",
+                stringsAsFactors = FALSE)
 
 urls <- data$rec_url
 
-###################################
-### Escritura de URLs
-###################################
 writeLines(urls, "urls.txt")
-
 ###################################
 ### Verificacion de codigo
 ###################################
-system("/home/luis/Documents/Presidencia/MiningDatosGob/url_test/url_test.sh")
+urlstatus <- list()
+for(i in 1:length(urls)){
+    instr  <- paste0("curl -k -o /dev/null --silent --head --write-out '%{http_code}' ",
+                    urls[i])
+    ## Try with timeout
+    tryCatch({
+        evalWithTimeout({
+            code   <- system(instr)
+        }, timeout = 2);
+    }, TimeoutException = function(ex) {
+        code <- "000"
+        cat("Timeout. Skipping.\n");
+})
+    urlstatus[[i]] <- c(urls[i], code)
+    print(i)
+}
+
 
 ###################################
 ### Clasificación de ligas
@@ -27,11 +43,12 @@ system("/home/luis/Documents/Presidencia/MiningDatosGob/url_test/url_test.sh")
 urls_code <- read.csv("urlstatus.csv", stringsAsFactors = FALSE,
                      header = FALSE)
 names(urls_code) <- c("url","code")
+
 ## Función para clasificar una liga
 change_lable <- function(lable){
     class <- "no disponible"
     if(str_detect(lable, "20[0-3]") |
-       str_detect(lable, "00.") |
+       str_detect(lable, "406") |
        str_detect(lable, "300")     |
        str_detect(lable, "30[2-7]") |
        str_detect(lable, "50.")
